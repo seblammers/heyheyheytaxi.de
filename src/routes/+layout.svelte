76d20f1f97e842ca6ae24edb2ef83f077a17e179
Sebastian Lammers <sebastian@nussknagger.com>
2025-12-01 21:59:09 +0100

    Add Zilla Slab font and update layout and page content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import '@fontsource/zilla-slab/400-italic.css';
+	import '@fontsource/zilla-slab/700-italic.css';
+	import '@fontsource/zilla-slab/400.css';
+	import '@fontsource/zilla-slab/700.css';
+
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -7,6 +12,9 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Hey, hey, hey, Taxi!</title>
 </svelte:head>
 
-{@render children()}
+<main>
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="flex flex-col items-center justify-center mx-auto w-screen h-screen max-w-prose">
+	<div>
+		<h1 class="text-4xl font-bold">Hey, hey, hey, Taxi!</h1>
+		<p class="text-xl">
+			Ich steige in ein Taxi, und das Taxi ist eine Internetseite und die Internetseite wird das
+			Zuhause für all eure Taxi-Geschichten sein.
+		</p>
+	</div>
+</section>
